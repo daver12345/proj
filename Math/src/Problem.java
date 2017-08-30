0cd593cc31a37d7ec5789fa94ddb1c54b9035297
daver12345 <31458212+daver12345@users.noreply.github.com>
2017-08-29 22:37:50 -0400

    Add files via upload

--- Math/src/Problem.java
+++ Math/src/Problem.java
@@ -1,22 +1,21 @@
 
 public class Problem {
 
-	int diameter = 4;
-	int tpr = 1012;
+	int diameter = 4; // Diameter of wheel
+	int tpr = 1012; // Ticks per revolution of wheel
 	
 		public void calculator(int distanceTraveled) {
 			
-			double circum = diameter * 3.14;
-			double tpi = tpr/360d;
-			double tpi2 = tpi * circum;
-			double output = tpi2 * distanceTraveled;
-			double roundOff = Math.round(output * 1000.0) / 1000.0;
-			System.out.println(roundOff + " ticks is the amount of ticks for " + distanceTraveled + " inches traveled by the wheel.");
+			double circum = diameter * 3.14; // Calculates circumference
+			double tpi = (tpr/360d) * circum; // Converts ticks per revolution to ticks per inch
+			double output = tpi * distanceTraveled; // Multiplies ticks per inch by distance in inches
+			double roundOff = Math.round(output * 1000.0) / 1000.0; // Rounds the final number to the 1000's place
+			System.out.println(roundOff + " ticks is the amount of ticks for " + distanceTraveled + " inch(es) traveled by the wheel."); // Prints final answer
 		}
 	
 	public static void main(String[] args) {
-		Problem tprConversion = new Problem(); 
-		tprConversion.calculator(2); //Input a distance traveled in between the ().
+		Problem tprConversion = new Problem(); // Creates a new object that allows the method calculator() to be invoked
+		tprConversion.calculator(1); //Input a distance traveled in between the ()
 	}
 
 }
